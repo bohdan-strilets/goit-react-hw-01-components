b044b9d8693269ae746d56a2d1f91b8bb3891707
Bohdan Strilets <bohdan.strilets@gmail.com>
2022-02-13 04:42:04 +0100

    social profile card

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import SocialProfile from "./components/SocialProfile/SocialProfile";
+import user from "./data/user.json";
+
 export default function App() {
-  return <div></div>;
+  return (
+    <div>
+      <SocialProfile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+    </div>
+  );
 }
--- src/components/SocialProfile/SocialProfile.jsx
+++ src/components/SocialProfile/SocialProfile.jsx
@@ -1,5 +1,55 @@
 import PropTypes from "prop-types";
+import defaultImage from "./defaultImage.png";
+import s from "../SocialProfile/SocialProfile.module.css";
 
-export default function SocialProfile(props) {}
+export default function Profile({
+  username = "User Name",
+  tag,
+  location,
+  avatar = defaultImage,
+  followers = 0,
+  views = 0,
+  likes = 0,
+}) {
+  return (
+    <div className={s.profile}>
+      <div className={s.description}>
+        <img
+          src={avatar}
+          alt="User avatar"
+          width={100}
+          height={100}
+          className={s.avatar}
+        />
+        <p className={s.name}>{username}</p>
+        <p className={s.tag}>@{tag}</p>
+        <p className={s.location}>{location}</p>
+      </div>
 
-const propTypes = {};
+      <ul className={s.stats}>
+        <li className={s.item}>
+          <span className={s.label}>Followers</span>
+          <span className={s.quantity}>{followers}</span>
+        </li>
+        <li className={s.item}>
+          <span className={s.label}>Views</span>
+          <span className={s.quantity}>{views}</span>
+        </li>
+        <li className={s.item}>
+          <span className={s.label}>Likes</span>
+          <span className={s.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+Profile.prototype = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string,
+  followers: PropTypes.number,
+  views: PropTypes.number,
+  likes: PropTypes.number,
+};
